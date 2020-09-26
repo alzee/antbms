921c3761741faaf135a62e9df4b9112a4095b65f
Al Zee <z@alz.ee>
2020-09-26 16:29:46 +0800

    pages/status/index.wxml start for template

--- pages/status/index.js
+++ pages/status/index.js
@@ -12,7 +12,15 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        var that = this
+        wx.request({
+            url: getApp().globalData.url + '/GetSingleEquipment',
+            success(res){
+                console.log(res.data.Data);
+                console.log(getApp().globalData.url);
+                that.setData(res.data.Data);
+            }
+        })
     },
 
     /**
