cd15834f89f2472a81f02d2f48f29e089e5b0c4b
Al Zee <z@alz.ee>
2020-09-28 17:44:35 +0800

    create map

--- pages/map/index.js
+++ pages/map/index.js
@@ -3,8 +3,47 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    lat: 32.649136,
+    lon: 110.830931,
+    //markers: [{
+    //  //iconPath: "/resources/others.png",
+    //  id: 0,
+    //  latitude: 32.649136,
+    //  longitude: 110.830931,
+    //  width: 50,
+    //  height: 50
+    //}],
+    polyline: [{
+      points: [{
+        longitude: 113.324520,
+        latitude: 23.099994
+      }, {
+        longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color:"#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      //iconPath: '/resources/location.png',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
   },
-  onLoad: function () {
+  regionchange(e) {
+    console.log(e.type)
+  },
+  markertap(e) {
+    console.log(e.detail.markerId)
+  },
+  controltap(e) {
+    console.log(e.detail.controlId)
   }
 })
